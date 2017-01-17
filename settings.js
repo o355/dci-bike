2e19e5306f2f2f22ba2b1b9ca877a6f9d2433507
Benchmaker <maxbarkman@optonline.net>
2017-01-17 13:23:29 -0500

    Created Settings.js
    Added features

---
+++ settings.js
@@ -0,0 +1,7 @@
+/*/ Server Settings will be uploaded later within the project 
+-Features
+  -Connection throttling 
+  -Database for Each class
+  -Organization for Each class
+  -User connection data Storge
+  /*/
