5431429fb46f794d90c6edfd2ba9d4a1a6c8abe4
Benchmaker <maxbarkman@optonline.net>
2017-01-17 08:01:04 -0500

    Updated Socket.js
    Removed old Socket server and added new

--- socket.js
+++ socket.js
@@ -1,16 +1,30 @@
-var repl = require('repl')
-var net = require('net')
+var net = require('net');
 
-net.createServer(function (socket) {
-  var r = repl.start({
-      prompt: 'socket '+socket.remoteAddress+':'+socket.remotePort+'> '
-    , input: socket
-    , output: socket
-    , terminal: true
-    , useGlobal: false
-  })
-  r.on('exit', function () {
-    socket.end()
-  })
-  r.context.socket = socket
-}).listen(8088)
+var HOST = '127.0.0.1';
+var PORT = 8088;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
