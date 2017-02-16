18952d4debe4204d58e59d513a17234834603a0b
Benchmaker <maxbarkman@optonline.net>
2017-02-16 10:24:46 -0500

    Created logon.py

---
+++ login.py
@@ -0,0 +1,31 @@
+from appJar import gui
+
+# create the GUI & set a title
+app = gui("Login to DCI-Bike 1!")
+.showSplash(text, fill='red', stripe='black', fg='white', font=44)
+
+def press(btnName):
+    if btnName == "Cancel":
+        app.stop()
+        return
+
+    if app.getEntry("userEnt") == "testuser":
+        if app.getEntry("passEnt") == "testpassword":
+            app.infoBox("Success", "Congratulations, you are logged in!")
+        else:
+            app.errorBox("User login Failed!", "Invalid password")
+    else:
+        app.errorBox("User Login Failed!", "Invalid username")
+
+# add labels & entries
+# in the correct row & column
+app.addLabel("userLab", "Username:", 0, 0)
+app.addEntry("userEnt", 0, 1)
+app.addLabel("passLab", "Password:", 1, 0)
+app.addSecretEntry("passEnt", 1, 1)
+
+# changed this line to call a function
+app.addButtons( ["Submit", "Cancel"], press, colspan=2)
+
+# add some enhancements
+app.setFocus("userEnt")
